5b8f507e6d06aa90e2e30c0788dcca282afe47b5
SandyWoolman <SandyWoolman@users.noreply.github.com>
2020-09-16 15:11:35 +0200

    gros chat blanc

--- main.cpp
+++ main.cpp
@@ -2,17 +2,38 @@
 
 int main()
 {
-	std::cout << "vous croisez un gros chat blanc\n";
-	std::cout << "combien de carresses vous lui faites?\n";
+	std::cout << "Vous croisez un gros chat blanc.\n";
+	std::cout << "Combien de carresses vous lui faites?\n";
 	int ronronnement;
 	std::cin >> ronronnement;
 	if (ronronnement < 5)
 	{
-		std::cout << "le chat vous snob";
+		std::cout << "Le chat vous snob\n";\
+
+		if ("Le chat vous snob\n");
+		std::cout << "Essayez vous encore?\n";
+		std::cout << "Oui 1\n";
+		std::cout << "Non 2\n";
+
+		int ronronnement2;
+		std::cin >> ronronnement2;
+		if (ronronnement2 == 2)
+		{
+			std::cout << "Le chat part. Mauvaise fin.\n";
+		}
+		if (ronronnement2 == 1)
+		{
+			std::cout << "Le chat vous aime. Bonne fin.\n";
+		}
+		if (ronronnement2 <= 0 >= 3);
+		{
+			std::cout << "Petit batard, on a dit OUI 1 et NON 2. Le monde explose. Bravo. J'espere que tu es fier de toi.\n";
+		}
 	}
 	if (ronronnement >= 5)
 	{
-		std::cout << "le chat vous adopte";
+		std::cout << "Le chat vous adopte.";
 	}
+	
 
 }
