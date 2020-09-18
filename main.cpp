1cac7a68bb17810e1ef035313d38544a1dce1a0b
SandyWoolman <71042617+SandyWoolman@users.noreply.github.com>
2020-09-18 14:13:39 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,65 +1,103 @@
 #include <iostream>
+#include <string>
+#include <array>
+using namespace std;
 
+enum class State {
+	INTRO,
+	DEHORS,
+	POMMES,
+	FIN,
+	AUBERGE,
+	AUBERGISTE,
+	MONTE,
+	CHAMBRE,
+};
+// VARIABLES GLOBALES
+State CurrentState = State::INTRO;
+string PlayerName;
+
+State Intro() {
+	cout << "Bien le bonjour, cher aventurier. Quel est votre nom?\n";
+	cin >> PlayerName;
+	cout << "Ravi de vous rencontrer, " << PlayerName << ".\n "
+		<< "Vous avez perdu l'amulette de la prophecie dans votre chambre de l'auberge hier soir.\n"
+		<< "Vos compagnions attendent que vous alliez la recuperer.\n"
+		<< "Vous etes dehors."; 
+	return State::DEHORS;
+}
+State Chambre() {	
+	cout << "Vous etes dans la chambre. Vous voyez le lit (1) et la fenetre (2).\n";
+	cout << "Que faites-vous?\n";
+	string chambre;
+	int amulette;
+	cin >> amulette;
+	switch (amulette) {
+		case 1:
+			cout << "Vous retrouvez l'amulette. Les Hommes-Patates pourront etre battu.\n FIN";
+			return State::FIN;
+			break;
+		case 2:
+			cout << "Vous sautez par la fenetre et mourrez. Oups!";
+			return State::FIN;
+			break;
+		default:
+			cout << "L'amulette est perdue a jamais. Le seigneur des Hommes-Patates pourra dominer le monde sans probleme a cause de " << PlayerName << ".";
+			break;
+	}
+}
+State Aubergiste() {
+	int etage;
+	cin >> etage;
+	switch (etage){
+		case 1:
+			cout << "L'aubergiste vous remercie de vous etre annoncer. Vous allez dans votre chambre.\n";
+			return State::CHAMBRE;
+			break;
+		case 2:
+			cout << "Vous etes malpoli et vous allez dans votre chambre.\n";
+			return State::CHAMBRE;
+			break;
+		default:
+		return State::FIN;
+		break;
+	}
+}
+State Dehors() {
+	cout << "Vous etes dehors de l'auberge. \n"
+		<< "Que faites-vous?\n "
+		<< "Je rentre dans l'auberge. 1\n "
+		<< "Je n'ai pas envie d'aller chercher l'amulette. Les prophecies sont pour les tarlouzes. 2\n";
+	int Choice;
+	cin >> Choice;
+	switch(Choice) {
+		case 1:			
+			cout << "Vous rentrez dans l'auberge. Vous voyez le proprietaire en train de parlez avec un homme à capuche, la charmante serveuse d'hier soir balaye le sol.\n Que faites-vous? \n Je m'anonce à l'aubergiste. (1)\n Je monte dans la chambre ou j'ai dormi hier soir. (2)\n";
+			Aubergiste();
+			return State::AUBERGE;
+			break;
+		default:
+			cout << "L'amulette est perdue a jamais. Le seigneur des Hommes-Patates pourra dominer le monde sans probleme a cause de " << PlayerName << ".";
+			return State::FIN;
+			break;
+	}
+}
 int main()
 {
-	std::cout << "Vous croisez un gros chat blanc.\n";
-	std::cout << "Combien de carresses vous lui faites?\n";
-	int ronronnement;
-	std::cin >> ronronnement;
-	if (ronronnement < 5)
-	{
-		std::cout << "Le chat blanc vous snob\n";\
-
-		if ("Le chat blanc vous snob\n")
-		{
-			std::cout << "Essayez vous encore?\n";
-			std::cout << "Oui 1\n";
-			std::cout << "Non 2\n";
-
-			int ronronnement2;
-			std::cin >> ronronnement2;
-			if (ronronnement2 == 1)
-			{
-				std::cout << "Le chat blanc vous aime. Fin 1.\n";
-			}
-			else if (ronronnement2 == 2)
-			{
-				std::cout << "Le chat blanc part. Mauvaise fin 1.\n";
-			}
-			else
-			{
-				std::cout << "Petit batard, le monde explose. Bravo. J'espere que tu es fier de toi. Fin secrete\n";
-			}
-		}
-	}
-	if (ronronnement >= 5)
-	{
-		std::cout << "Le chat blanc vous adopte.\n"; \
-
-		if ("Le chat blanc vous adopte.\n") {
-			std::cout << "Un chat roux arrive.\n";
-			std::cout << "Que faites-vous?\n";
-			std::cout << "Je le caresse. 1\n";
-			std::cout << "Je l'ignore. 2\n";
-			std::cout << "Je lui donne a manger. 3\n";
-
-			int ronronnement3;
-			std::cin >> ronronnement3;
-			switch (ronronnement3)
-			{
-				case 1: 
-					std::cout << "Les ronronnements du chat roux ressemblent a des grincements. Il vous aime bien. Fin 2\n";
-					break;
-				case 2: 
-					std::cout << "Le chat roux n'apprecie pas. Il vous saute a la gorge. Vous etes mort. Mauvaise fin 2\n";
-					break;
-				case 3:
-					std::cout << "Le chat roux devore la viande et se retourne vers vous.\n";
-					break;
-				default:
-					std::cout << "Petit batard, le monde explose. Bravo. J'espere que tu es fier de toi. Fin secrete \n";
-					break;
-			}
+	while (true) {
+		switch (CurrentState) {
+		case State::INTRO:
+			CurrentState = Intro();
+			break;
+		case State::DEHORS:
+			CurrentState = Dehors();
+			break;
+		case State::AUBERGE:
+			CurrentState = Aubergiste();
+			break;
+		case State::CHAMBRE:
+			CurrentState = Chambre();
+			break;
 		}
 	}
 }
